79fe7259247b42e0bf402a9c25bba035c7e699d4
phamtdat <>
2020-12-18 12:39:34 +0100

    Avoid generating codegen adapter

--- app/src/main/java/com/example/moshimultiplejsondemo/MyModel.kt
+++ app/src/main/java/com/example/moshimultiplejsondemo/MyModel.kt
@@ -3,7 +3,8 @@ package com.example.moshimultiplejsondemo
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
-@JsonClass(generateAdapter = true)
+// tell codegen to not generate adapter, as we provide our own custom adapter
+@JsonClass(generateAdapter = false)
 data class MyModel(
     @Json(name = "Id") val Id: Long,
     @Json(name = "image") val images: List<String>
